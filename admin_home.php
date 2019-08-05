c749157d9872562c7bfd7aefa7bd5f0104150a37
sujaany <ssujaany09@gmail.com>
2019-08-05 17:10:14 +0530

    first commit

---
+++ admin_home.php
@@ -0,0 +1,30 @@
+<?php
+include "database.php";
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Tutor Joe's </title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+		<body>
+			<?php include "navbar.php";?><br>
+			<img src="..." style="margin-left:90px;" class="sha">
+			<div id="selection">
+				<?php include"sidebar.php";?><br>
+				<div class="content">
+					<h3 class="text"> Welcome <?php echo $_SESSION["ANAME"];?></h3><br><hr><br>
+					<h3> School Information</h3><br>
+					<img src="..." class="">
+					<p class="para">
+						jnfjbn
+</p>
+<p class="para">
+	cdcnjkn
+</p>
+</body>
+</head>
+
+</html>
---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+include "database.php";
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>School Management System </title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body class="back">
+	<?php include "navbar.php"?>
+	<img src="https://cdn.pixabay.com/photo/2017/09/18/11/14/school-2761394_960_720.jpg" width=800 />
+
+	<div class="login">
+		<h1 class="heading">Admin Login</h1>
+		<div class="log">
+		
+		<?php
+		if(isset($_POST["login"]))
+		{
+			$sql ="select * from admin where ANAME ='{$_POST["aname"]}' and APASS='{$_POST["apass"]}'";
+			$res =$db ->query($sql);
+			if($res -> num_rows >0)
+			{
+				$ro =$res ->fetch_assoc();
+				$_SESSION["AID"] =$ro["AID"];
+				$_SESSION["ANAME"] =$ro["ANAME"];
+				echo "<script>window.open('admin_home.php','_self');</script>";
+			}
+			else
+			{
+				echo "<div class='error'>Invalid UserName or Password </div>";
+			}	
+		}
+		if(isset($_GET["mes"]))
+		{
+			echo "<div class='error'>{$_GET["mes"]}</div>";
+		}
+		?>
+			<form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>"
+				<label>User Name</label><br>
+				<input type="text" name="aname" required class="input"><br><br>
+				<label>Password</label><br>
+				<input type="password" name="apass" required class="input"><br>
+				<button type="submit" class="btn" name="login">Login here </button>
+			</form>
+</div>
+</div>
+
+<div class="footer">
+<footer>
+<p>Copyright @School Management </p>
+</footer>
+</div>
+
+</body>
+</html>
